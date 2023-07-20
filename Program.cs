2ec2c4ed7518e8e1eee26b3ba3aceacb1c82fe2c
Jeyanandhini-K <110195166+Jeyanandhini-K@users.noreply.github.com>
2023-07-20 10:56:23 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int a;
+            int[] cars = { 10, 20, 60, 45, 748 };
+           Console.Write("Enter the index number");
+
+            a = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= cars.Length; i++)
+           
+            {
+
+                Console.WriteLine(i);
+                
+            }
+            
+
+        }
+    }
+}
